64c064dfe12e901b037c2a6525bead42cc392c31
laura <lauramolinalopez02@gmail.com>
2020-03-04 19:53:13 +0100

    bottom navigatin bar añadido al first screen una vez hecho el log in

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,18 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:graus_upc/screens/login_page.dart';
 import 'package:graus_upc/screens/sign_in.dart';
+import 'package:graus_upc/screens/HomeScreen.dart';
 //import 'package:google_sign_in/google_sign_in.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  int _selectedIndex = 2;
+
+  void _onItemTapped(int index) {
+    if (index == 0) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      );
+    } else if (index == 1) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      );
+    }
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 13,
+          unselectedLabelStyle: TextStyle(fontSize: 11, color: Colors.red),
+          unselectedIconTheme: IconThemeData(size: 12, color: Colors.grey[600]),
+          backgroundColor: Colors.white,
+          iconSize: 24,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today),
+              title: Text("CALENDAR"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+              ),
+              title: Text("HOME"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text("PROFILE"),
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.black,
+          onTap: _onItemTapped,
+        ),
       body: Container(
+        
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topRight,
             end: Alignment.bottomLeft,
-            colors: [Colors.blue[100], Colors.blue[400]],
+            colors: [Colors.blue[100], Colors.white],
           ),
         ),
         child: Center(
@@ -61,7 +115,10 @@ class FirstScreen extends StatelessWidget {
               RaisedButton(
                 onPressed: () {
                   signOutGoogle();
-                  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) {return LoginPage();}), ModalRoute.withName('/'));
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) {
+                    return LoginPage();
+                  }), ModalRoute.withName('/'));
                 },
                 color: Colors.deepPurple,
                 child: Padding(
