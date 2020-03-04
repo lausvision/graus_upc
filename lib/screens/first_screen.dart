2a29dbcd6d8c0429e304de22bd4b8cf98ea9ecdd
laura <lauramolinalopez02@gmail.com>
2020-03-04 20:43:36 +0100

    redissenyant firstScreen, afegit imatge de marble en png en els assets, afegit tambe al yaml

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+//import 'package:graus_upc/screens/EditProfileScreen.dart';
 import 'package:graus_upc/screens/login_page.dart';
 import 'package:graus_upc/screens/sign_in.dart';
 import 'package:graus_upc/screens/HomeScreen.dart';
@@ -33,8 +34,19 @@ class _FirstScreenState extends State<FirstScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
+    return Stack(
+          children: <Widget>[
+            Container(
+               decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/marble.png"), // <-- BACKGROUND IMAGE
+              fit: BoxFit.cover,
+            ),
+          ),
+            ),
+            Scaffold(
+        backgroundColor: Colors.blue[100],
+        bottomNavigationBar: BottomNavigationBar(
           selectedFontSize: 13,
           unselectedLabelStyle: TextStyle(fontSize: 11, color: Colors.red),
           unselectedIconTheme: IconThemeData(size: 12, color: Colors.grey[600]),
@@ -60,82 +72,106 @@ class _FirstScreenState extends State<FirstScreen> {
           selectedItemColor: Colors.black,
           onTap: _onItemTapped,
         ),
-      body: Container(
-        
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [Colors.blue[100], Colors.white],
+        appBar: AppBar(
+          elevation: 0,
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          // title: Text( 'Tu Perfil', style: TextStyle(color: Colors.teal), ),
+          backgroundColor: Colors.transparent,
+          leading: IconButton(
+            icon: Icon(Icons.exit_to_app),
+            tooltip: 'Sign out',
+            color: Colors.black,
+            onPressed: () {
+              signOutGoogle();
+              Navigator.of(context).pushAndRemoveUntil(
+                  MaterialPageRoute(builder: (context) {
+                return LoginPage();
+              }), ModalRoute.withName('/'));
+            },
           ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                  imageUrl,
-                ),
-                radius: 60,
-                backgroundColor: Colors.transparent,
-              ),
-              SizedBox(height: 40),
-              Text(
-                'NAME',
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black54),
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'EMAIL',
-                style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black54),
-              ),
-              Text(
-                email,
-                style: TextStyle(
-                    fontSize: 25,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 40),
-              RaisedButton(
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(Icons.edit),
+                tooltip: 'Edit profile',
+                color: Colors.black,
                 onPressed: () {
-                  signOutGoogle();
-                  Navigator.of(context).pushAndRemoveUntil(
-                      MaterialPageRoute(builder: (context) {
-                    return LoginPage();
-                  }), ModalRoute.withName('/'));
-                },
-                color: Colors.deepPurple,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Sign Out',
-                    style: TextStyle(fontSize: 25, color: Colors.white),
+                  /*
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => EditProfileScreen(
+                        userId,
+                        Objeto(
+                          'New name',
+                          'New username',
+                          'New email',
+                          '*****',
+                          '*****',
+                        ),
+                      ),
+                    ),
+                  );*/
+                })
+          ],
+        ),
+        body: Container(
+          /* decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Colors.blue[100], Colors.white],
+            ),
+          ),*/
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: NetworkImage(
+                    imageUrl,
                   ),
+                  radius: 65,
+                  backgroundColor: Colors.transparent,
                 ),
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(40)),
-              )
-            ],
+                SizedBox(height: 40),
+                Text(
+                  'NAME',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54),
+                ),
+                Text(
+                  name,
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  'EMAIL',
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black54),
+                ),
+                Text(
+                  email,
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 40),
+              ],
+            ),
           ),
         ),
       ),
+
+          ] 
     );
   }
 }
