6a178e009f08c5b853891cc705d799f6435b84e7
laura <lauramolinalopez02@gmail.com>
2020-03-16 14:04:50 +0100

    main limpio

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:graus_upc/screens/HomeScreen.dart';
-import 'package:graus_upc/data/llegeix.dart';
+
+
 import 'package:graus_upc/screens/HomeScreen.dart';
 
 void main() => runApp(MyApp());
