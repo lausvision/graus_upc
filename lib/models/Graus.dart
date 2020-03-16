4ee28db4d364350ac312bd2ea0ef17c9fb3e9142
laura <lauramolinalopez02@gmail.com>
2020-03-16 09:43:33 +0100

    Graus class created

---
+++ lib/models/Graus.dart
@@ -0,0 +1,28 @@
+
+class Graus{
+  String _nom, _objectius, _modalitat, _link, _foto, _localitzacio, _branca, _ambit;
+  double _nota;
+
+  Graus(
+    this._nom,
+    this._objectius,
+    this._modalitat,
+    this._link,
+    this._foto,
+    this._localitzacio,
+    this._branca,
+    this._ambit,
+    this._nota
+  );
+ 
+  get nom => _nom;
+  get objectius => _objectius;
+  get modalitat => _modalitat;
+  get link => _link;
+  get foto => _foto;
+  get localitzacio => _localitzacio;
+  get branca => _branca;
+  get ambit => _ambit;
+  get nota => _nota;
+
+}
