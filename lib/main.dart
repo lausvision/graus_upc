79130e974daecb26be5552ad7f5d08ddd61d5259
laura <lauramolinalopez02@gmail.com>
2020-04-23 13:53:25 +0200

    Await/Splach Screen lograda

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graus_upc/screens/AwaitScreen.dart';
 import 'package:graus_upc/screens/HomeScreen.dart';
 //import 'package:graus_upc/screens/versio.dart';
 
@@ -8,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: HomeScreen()),
+      home: Scaffold(body: AwaitScreen()),
     );
   }
 }
--- lib/screens/AwaitScreen.dart
+++ lib/screens/AwaitScreen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:graus_upc/screens/HomeScreen.dart';
+import 'package:graus_upc/screens/first_screen.dart';
 
 class AwaitScreen extends StatefulWidget {
   @override
@@ -9,35 +10,26 @@ class AwaitScreen extends StatefulWidget {
 }
 
 class _AwaitScreenState extends State<AwaitScreen> {
-
- @override
- void initState() {
-   
+  @override
+  void initState() {
     super.initState();
-     Timer(Duration(seconds: 2), () => Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) {
-                return HomeScreen();
-              },
-            ),
-          ));
+    Timer(
+        Duration(seconds: 2),
+        () => Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return HomeScreen();
+                },
+              ),
+            ));
   }
-
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Container(
-     width: 400,
-child: Image.asset(
-                'assets/AwaitScreen.jpg',
-                fit: BoxFit.cover, 
-              ),
-
-      
-      ),
-      
-    );
-
+    return Scaffold(
+        body: Stack(alignment: AlignmentDirectional.center, children: <Widget>[
+      WallpaperFoto(),
+      Image(image: AssetImage("assets/logo.png"), height: 300),
+    ]));
   }
 }
