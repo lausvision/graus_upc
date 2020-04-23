4cbb7e5b9bcb9a27d2501f82185af13eac4fc027
laura <lauramolinalopez02@gmail.com>
2020-04-23 13:48:58 +0200

    awaitScreen 2 segundos

--- lib/screens/AwaitScreen.dart
+++ lib/screens/AwaitScreen.dart
@@ -1,6 +1,29 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class AwaitScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:graus_upc/screens/HomeScreen.dart';
+
+class AwaitScreen extends StatefulWidget {
+  @override
+  _AwaitScreenState createState() => _AwaitScreenState();
+}
+
+class _AwaitScreenState extends State<AwaitScreen> {
+
+ @override
+ void initState() {
+   
+    super.initState();
+     Timer(Duration(seconds: 2), () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return HomeScreen();
+              },
+            ),
+          ));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return  Center(
