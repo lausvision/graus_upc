d59d88e22835ef5caa752ed48ce73235fd066ac4
Marti Olle <martiollea@gmail.com>
2020-05-02 00:46:09 +0200

    Proba filtrat localització.

--- lib/filters/Loc.dart
+++ lib/filters/Loc.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:graus_upc/data/llegeix.dart';
 import 'package:graus_upc/screens/HomeScreen.dart';
 import 'package:graus_upc/screens/InfoScreen.dart';
 import 'package:graus_upc/screens/ProfileScreen.dart';
@@ -13,6 +15,7 @@ class Loc extends StatefulWidget {
 class _LocState extends State<Loc> {
   int _selectedIndex = 1;
   String valor;
+  String index;
 
   void _onItemTapped(int index) {
     if (index == 0) {
@@ -90,6 +93,7 @@ class _LocState extends State<Loc> {
                 style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
               ),
               _camp(context),
+              //Expanded(child: Locs(index)),
               DecoratedBox(
                 decoration: ShapeDecoration(
                     shape: StadiumBorder(), color: Colors.blue[200]),
@@ -130,7 +134,7 @@ class _LocState extends State<Loc> {
           });
         },
         decoration: InputDecoration(
-          hintText: "Search",
+          hintText: "Escriu el nom del centre a buscar.",
           prefixIcon: Icon(Icons.search),
           border: OutlineInputBorder(
             borderRadius: BorderRadius.all(Radius.circular(15.0)),
@@ -140,3 +144,105 @@ class _LocState extends State<Loc> {
     );
   }
 }
+/*
+class Locs extends StatefulWidget {
+  final String loc;
+  const Locs(this.loc);
+
+  @override
+  _LocsState createState() => _LocsState();
+}
+
+class _LocsState extends State<Locs> {
+  filtraLlista(List<String> locs) {
+    List<String> grausFiltrats = [];
+
+    if (widget.loc == null) {
+      grausFiltrats = locs;
+    } else {
+      for (int j = 0; j < locs.length; j++) {
+        final String nomMinuscules = locs[j].toLowerCase();
+        final String filtreMinuscules = widget.loc.toLowerCase();
+        if (nomMinuscules.indexOf(filtreMinuscules) != -1) {
+          grausFiltrats.add(locs[j]);
+        }
+      }
+    }
+    return grausFiltrats;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: true,
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('Graus').orderBy('nom').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return const Text('Loading...');
+
+          List<DocumentSnapshot> documents = snapshot.data.documents;
+          List<Grau> locs =[];
+              documents.map((doc) => Grau.fromFirestore(doc)).toList();
+          List<String> locsFiltre = filtraLlista(locs);
+
+          return ListView.builder(
+            itemExtent: 100,
+            itemCount: locsFiltre.length,
+            itemBuilder: (context, index) =>
+                _llista(context, locsFiltre[index]),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _llista(BuildContext context, String locsFiltre) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: InkWell(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    constraints: BoxConstraints(minWidth: 100, maxWidth: 270),
+                    child: Text(
+                      locsFiltre,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 17),
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    locsFiltre,
+                    style: TextStyle(fontSize: 14),
+                  ),
+                  Expanded(child: Container()),
+                  Container(
+                    child: Text(
+                      locsFiltre,
+                      textAlign: TextAlign.end,
+                      style: TextStyle(fontSize: 14),
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}*/
